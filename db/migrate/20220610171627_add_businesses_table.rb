ba504c52305b12b544ced6c7a8e5127787b4a6da
brady diamond <brady.diamond27@gmail.com>
2022-06-10 14:34:18 -0700

    troubleshoot migration

--- db/migrate/20220610171627_add_businesses_table.rb
+++ db/migrate/20220610171627_add_businesses_table.rb
@@ -3,6 +3,7 @@ class AddBusinessesTable < ActiveRecord::Migration[5.2]
     create_table :businesses do |t|
       t.column :name, :string
       t.column :location, :string
+      t.column :owner, :string
     end
   end
 end
