caa71629f1d4f1429fa30813ca8eca9722e135bf
brady diamond <brady.diamond27@gmail.com>
2022-06-10 15:55:52 -0700

    spec troubleshoot

--- spec/requests/delete_businesses_spec.rb
+++ spec/requests/delete_businesses_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe "delete a business route", :type => :request do
   let!(:businesses) { FactoryBot.create_list(:business, 20)}
   before do
-    delete "/businesses/1"
+    delete "/businesses/:1"
   expect(reponse).to have_http_status(200)
   expect(reponse.body).to eq("This business has been sucessfully deleted" )
   end
