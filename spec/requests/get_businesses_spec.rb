18561918404edeaf7b261f347ef81c245ad06e27
brady diamond <brady.diamond27@gmail.com>
2022-06-10 15:11:03 -0700

    add paginate get test

--- spec/requests/get_businesses_spec.rb
+++ spec/requests/get_businesses_spec.rb
@@ -3,14 +3,20 @@ require 'rails_helper'
 describe "get all paginated businesses route", :type => :request do
   let!(:businesses) { FactoryBot.create_list(:business, 20)}
 
-  before { get '/businesses'}
+  # before { get '/businesses'}
 
   it 'returns paginated businesses' do
+    get '/businesses'
     expect(JSON.parse(response.body).size).to eq(5)
   end
 
+  it 'returns second page of animals' do
+    get '/businesses?page=2'
+    expect(JSON.parse(response.body).size).to eq(5)
+  end
 
   it 'returns status code 200' do
+    get '/businesses'
     expect(response).to have_http_status(:success)
   end
 end 
